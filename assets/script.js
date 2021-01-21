9cf55a8c016d9a894ae217406afd382f8a9c2ae3
Cameron Harvey <charvey0@gmail.com>
2021-01-20 20:25:46 -0800

    added getHallScores() and checkResult(r, c) to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,18 @@
-// This is all the places for the hall of fame 
-const ordinalNumbers = ['1st','2nd','3rd','4th','5th','6th','7th','8th','9th','10th'];
+
+/*************************************************************
+*               DISPLAY #1 FUNCTIONS 
+*********************************************************** */
+
+
+
+
+
+
+
+
+/*************************************************************
+*               DISPLAY #2 FUNCTIONS 
+*********************************************************** */
 
 // INPUT: number of integers to be in the return array
 // OUTPUT: a random array from 1 to the input number
@@ -21,6 +34,22 @@ function randomIntArray(n){
     return rand;
 }
 
+
+
+
+// INPUT: (1) the user's response to a question (2) the correct response
+// OUTPUT: TRUE if correct, FALSE if not
+function checkResult(r, c){
+    var result = false;
+    if (r == c) {
+        result = true;
+    }
+    return result;            
+}
+
+console.log(checkResult(1, "1"));
+
+
 // INPUT: input is the question index
 // OUTPUT: correct response
 function askQuestion(i){
@@ -39,4 +68,31 @@ function askQuestion(i){
     return questions[i][1];
 }
 
-askQuestion(0);
+
+
+
+
+/*************************************************************
+*               DISPLAY #3 FUNCTIONS 
+*********************************************************** */
+
+// This is all the places for the hall of fame 
+const ordinalNumbers = ['1st','2nd','3rd','4th','5th','6th','7th','8th','9th','10th'];
+
+
+
+
+// INPUT: none
+// OUTPUT: an array of scores in the hall of fame in decending order
+function getHallScores(){
+    // get hallScores from cache
+    var hallScores = JSON.parse(localStorage.getItem("hallScores"));
+  
+    // check in hallScores is null
+    if (hallScores == null) {
+        hallScores = [];
+    }
+
+    return hallScores;
+}
+
